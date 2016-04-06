b5b30011fcd1cb693c869485daaa9147a43e99dd
yeibercano <yeibercano@gmail.com>
2016-04-06 15:04:47 -0700

    Troubleshooting reciept of HTML content.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,36 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers');
+var http = require('http-get');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  statusCode = 200;
+    // console.log('this is the req method:', req.method)
+
+// http.get('./public/index.html', function (err, res) {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+  
+//   console.log(res.code, res.headers);
+// });
+
+   //an option 
+   if(req.method === 'GET') {
+      // console.log('req url: ', req.url)
+      if(req.url === '/'){
+        res.writeHead(statusCode, {'Content-type': 'text/hmtl'});
+        fs.readFile('./public/index.html', function (err, html){
+          console.log('html and err:', html, err)
+        }); 
+        res.end('<input>')
+      } else  {
+        console.log('else')
+      }
+   }
+
+
 };
